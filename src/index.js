64f12e8c7f79278022dc3dba2086b03a48d9512b
Catalina McQuade <catalinamcquade@gmail.com>
2020-10-05 12:56:44 -0400

    setup access token

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ console.log("hello!");
 
 import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2F0YWxpbnVoIiwiYSI6ImNrZndxcGFnNDF1ZHcycW84dmQ1bW5kNnAifQ.569__A4CK5p3TKwMRAGUlQ";
 
 const map = new mapboxgl.Map({
-	container: "map",
-	center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-	zoom: 12, // starting zoom
-	style: "mapbox://styles/mapbox/streets-v10", // mapbox has lots of different map styles available.
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10", // mapbox has lots of different map styles available.
 });
